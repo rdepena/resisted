bca00047d46bf65f1415dc41830065e2fc154007
Ricardo De Pena <ricardo.depena@gmail.com>
2013-12-28 14:16:08 -0500

    added support for 5 striped resistors.

--- src/resisted.service.js
+++ src/resisted.service.js
@@ -2,9 +2,16 @@
     'use strict';
 
     angular.module('resisted.service', [])
-    .factory('resistedGameService', function () {
-
+    .factory('resistedGameService', function (colorValues) {
         var my = {};
+        //only take those colors that have value.
+        var valueColors = colorValues.filter(function (color) {
+            return (color.value);
+        });
+        //only take colors with a valid multipliyer
+        var multiplierColors = colorValues.filter(function (color) {
+            return (color.multiplier);
+        });
 
         var getRandomInt = function (min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -15,46 +22,40 @@
         };
 
         var calculateResistance = function (colors) {
-            //we obtain the first two digits.
-            var firstDigits = "" +  colors[0].value +  colors[1].value;
+            var firstDigits = "";
+            angular.forEach(colors, function (color, i) {
+                //append the digits of the colors array, except the last color
+                if(i === colors.length -1) {
+                    return;
+                }
+                firstDigits = firstDigits + "" + color.value;
+            });
 
-            //then we apply the multipliyer:
-            var resistance = parseInt(firstDigits, 10) * colors[2].multiplier;
+            //take the last color's multiplier.
+            var resistance = parseInt(firstDigits, 10) * colors[colors.length -1].multiplier;
             return Math.round(resistance * 100) / 100;
         };
 
-        my.generateResistor = function (colors) {
+        my.generateResistor = function () {
             var randomResistorColors = [];
-            var i;
-            var assignedColor;
-            
-            //only take those colors that have value.
-            var valueColors = colors.filter(function (color) {
-                return (color.value);
-            });
-
-            //only take colors with a valid multipliyer
-            var multiplierColors = colors.filter(function (color) {
-                return (color.multiplier);
-            });
+            //get the resistor stripe count.
+            var stripeCount = getRandomInt(3,4);
 
             //generate the resistor.
-            for(i = 0; i < 3; i++){
-                if (i < 2) {
+            for(var i = 0; i < stripeCount; i++){
+                if (i < stripeCount -1) {
                     randomResistorColors.push(getRandomColor(valueColors));
                 } else {
                     randomResistorColors.push(getRandomColor(multiplierColors));
-                } 
-                
+                }
             }
-            var resistor = {
+
+            return {
                 colors : randomResistorColors,
-                resistance : calculateResistance(randomResistorColors) 
+                resistance : calculateResistance(randomResistorColors)
             };
-            return resistor;
         };
 
         return my;
     });
-
 }());
